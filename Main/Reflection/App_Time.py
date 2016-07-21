e912b77387fd864ca0454f5c34386eca3b109a9a
Mutad0r <Maarek.Aabmets@gmail.com>
2016-07-22 00:30:24 +0300

    Add Move Animation

--- Main/Reflection/App_Time.py
+++ Main/Reflection/App_Time.py
@@ -1,6 +1,6 @@
 from tkinter import*
 
-from Config import*
+import Cfg
 from time import*
 
 
@@ -9,9 +9,16 @@ class AppC_Time(Frame):
 
 	#### Initiate object variables
 	# Variables for system operation
-		self.X = X
-		self.Y = Y
+		self.Target_X = X
+		self.Target_Y = Y
+		self.Xmove=0
+		self.Ymove=0
+		self.speedy=2
+		self.speedx=2
 		self.name = "Clock"
+		self.hardheight = 138
+		self.hardwidth = 168
+		
 
 	# Class specific variables for object operation
 			# Create current time list
@@ -32,7 +39,7 @@ class AppC_Time(Frame):
 			# Create frame for the app and place at given coordinates
 		Frame.__init__(self, parent)
 		Frame.config(self, bg="#000000")
-		self.place(x=self.X, y=self.Y)
+		self.place(x=X, y=Y)
 
 	
 	### Draw the app
@@ -53,6 +60,40 @@ class AppC_Time(Frame):
 		self.timelist[0].set("{:02d}:{:02d}".format(self.timestringlist[3],self.timestringlist[4],))
 		self.timelist[1].set(self.daylist[self.timestringlist[6]])
 		self.timelist[2].set("{:02d}.{:02d}.{:d}".format(self.timestringlist[2],self.timestringlist[1],self.timestringlist[0]))
+		
+
+		self.speedy = abs(self.Target_Y - self.winfo_y())/10
+
+		self.speedx = abs(self.Target_X - self.winfo_x())/10
+
+		if abs(self.Target_Y - self.winfo_y()) <= 2:
+			self.speedy = abs(self.Target_Y - self.winfo_y())
+
+		if abs(self.Target_X - self.winfo_x()) <= 2:
+			self.speedx = abs(self.Target_X - self.winfo_x())
+
+		if self.Target_X < self.winfo_x():
+			self.Xmove=-self.speedx
+		elif self.Target_X > self.winfo_x():
+			self.Xmove=self.speedx
+		elif self.Target_X == self.winfo_x():
+			self.Xmove=0
+
+		if self.Target_Y < self.winfo_y():
+			self.Ymove=-self.speedy
+		elif self.Target_Y > self.winfo_y():
+			self.Ymove=self.speedy
+		elif self.Target_Y == self.winfo_y():
+			self.Ymove=0
+
+
+		
+
+
+		self.place(x=self.winfo_x()+self.Xmove,y=self.winfo_y()+self.Ymove)
+
+
+
 
 	def loophandler1000(self):
 		pass

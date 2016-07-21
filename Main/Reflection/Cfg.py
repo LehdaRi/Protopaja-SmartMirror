65087d799e65debd01e266052e6b7b1bbe29d967
Mutad0r <Maarek.Aabmets@gmail.com>
2016-07-22 00:30:48 +0300

    Initial commit of new global variables

---
+++ Main/Reflection/Cfg.py
@@ -0,0 +1,10 @@
+# This list will contain all the active applications
+app_list = []
+
+# These are global variables
+debugger = None
+root = None
+active_user = None
+
+# This list contains the names of all possible applications
+apps = ["Clock", "Calendar"]
